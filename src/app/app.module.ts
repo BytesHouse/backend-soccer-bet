67825a4c1961a48ab30e6e7958e8db4d2df21e19
a.lihtgolt <a.lihtgolt@gmail.com>
2024-07-05 19:14:28 +0300

    JWT token implement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,30 @@ import { AppService } from './app.service';
 import { CompetitionModule } from 'src/competition/competition.module';
 import { ProvidersModule } from 'src/providers/providers.module';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from 'src/modules/users/users.module';
+import { AuthModule } from 'src/modules/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/entities/user.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PG_HOST,
+      port: +process.env.PG_PORT,
+      username: process.env.PG_USERNAME,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DB_NAME,
+      // entities: [UserEntity],
+      synchronize: true,
+    }),
     CompetitionModule,
     ProvidersModule,
+    UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
